178aae85690ecca2c2b19233f2a75d87313ec03a
karina-rodriguez <karminer60@gmail.com>
2021-05-26 12:30:26 -0700

    made style edits and added comments

--- movie-app/src/App.js
+++ movie-app/src/App.js
@@ -4,14 +4,16 @@ import './App.css';
 import MovieList from './MovieList.js';
 
 import axios from 'axios'
-import React,{ useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
+
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 
 
 function App() {
   const [movieList, setMovieList] = useState(null);
-  
-  
+
+
 
   //axios is used to make HTTP requests -  HTTP is the protocol used in the world wide web that determines
   //how messages are formatted and transmitted, and what actions web servers and browsers should take in 
@@ -20,34 +22,36 @@ function App() {
   //Fetch API provides a JavaScript interface for accessing and manipulating parts of the HTTP pipeline, 
   //such as requests and responses; built into the browser
   //Provides a global fetch() method that provides an easy, logical way to fetch resources asynchrously across the network
-  
 
-    //useEffect being used so that is request only gets called when paramater changes 
-    const getMovies = () => {
-      axios.get("https://6049e293fb5dcc001796aba6.mockapi.io/movies")
+
+  //useEffect being used so that is request only gets called when paramater changes 
+  const getMovies = () => {
+    axios.get("https://6049e293fb5dcc001796aba6.mockapi.io/movies")
 
       //callback function to handle the response
       //setMovieList to fetched data
       .then(res => setMovieList(res.data))
-      
+
       //for case in which we don't get response
       .catch(err => console.log(err.response))
 
-    }
+  }
 
-    useEffect(() => {
-      getMovies();
-    }, [])
+  useEffect(() => {
+    getMovies();
+  }, [])
 
-    
-    return (
-      <div className="App">
-    
-      
-          <MovieList movies={movieList} />
+
+  return (
+    <div className="App">
+      <div className="container">
+
+        <MovieList movies={movieList} />
         
       </div>
-    );
+
+    </div>
+  );
 }
 
 
--- movie-app/src/MovieList.js
+++ movie-app/src/MovieList.js
@@ -1,4 +1,5 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
+
 
 /*- Year
   - Rating
@@ -8,91 +9,104 @@ import React, {useState} from 'react'
 
 
 //passing state through props from App
-export default function MovieList(props){
-    const {movies} = props; 
+export default function MovieList(props) {
+    const { movies } = props;
     const [searchValue, setSearchValue] = useState('');
 
     const onChangeHandler = e => {
         setSearchValue(e.target.value.toLowerCase())
     }
-
+    //filtered variable checks for no movies list, else filters for searchValue set in onChangeHandler through user input event
+    //filtered variable is made available to provide 'no results message' in return
     const filtered = movies === null ? null : movies.filter(movie => movie.title.toLowerCase().includes(searchValue))
-  
+
     //state in input changed through onChange event and onChangeHandler, event.target.value has what it is changed to; input is a controlled component
-    return(
+    return (
         <>
-            <div className="search-bar">
-                <input
-                type="text"
-                onChange={onChangeHandler}
-                name="searchValue"
-                value={searchValue}
-                placeholder="Filter titles"
-                id="searchBar"
-                />
+            <h1>Top Movies</h1>
+            <div className="search-bar" style={{ background: "#eee", paddingBottom: "15px", paddingLeft: "15px" }}>
+                <div className="input-group"  >
+                    <label for="searchBar" className="form-label">Filter Title</label>
+                    <input
+                        className="form-control"
+                        type="text"
+                        onChange={onChangeHandler}
+                        name="searchValue"
+                        value={searchValue}
+                        placeholder="Filter titles"
+                        id="searchBar"
+
+
+                    />
+                </div>
             </div>
-        
-        <table className="movie-list">
 
-            
 
-            <tr>
-                <th>Image</th> 
-                <th>Title</th> 
-                <th>Year</th>
-                <th>Rating</th> 
-                <th>Runtime</th> 
-                <th>Votes</th> 
-                <th>Genres</th>
-            </tr>
-        {   //conditional (ternary expression) in case movies are not loaded yet
-            filtered === null ? <tr><td>Loading data...</td></tr> :
-            filtered.length === 0 ? <tr><td>No records match your search.</td></tr> :
-            filtered.map(movie => (
-            <tr>
-                <td>
-                    <img src={movie.imageUrl} />
-                    
-                </td>
-                <td>
-                    <a href={movie.url}>
-                        <h2>{movie.title}</h2>
-                    </a>
+            <table className="table">
 
-                    <p>
-                        {movie.description}
-                    </p>
-                </td>
 
-                <td>
-                    <p>{movie.year}</p> 
-                    
-                </td>
+                <thead>
+                    <tr>
+                        <th>Image</th>
+                        <th>Title</th>
+                        <th>Year</th>
+                        <th>Rating</th>
+                        <th>Runtime</th>
+                        <th>Votes</th>
+                        <th>Genres</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {   //conditional (ternary expression) in case movies are not loaded yet
+                        //+ conditional (ternary expression) in case there are no results for searched movies
+                        filtered === null ? <tr><td>Loading data...</td></tr> :
+                            filtered.length === 0 ? <tr><td>No records match your search.</td></tr> :
+                                filtered.map(movie => (
+                                    <tr>
+                                        <td>
+                                            <img src={movie.imageUrl} />
 
-                <td>
-                    <p>{movie.rating}</p> 
-                    
-                </td>
+                                        </td>
+                                        <td>
+                                            <a href={movie.url}>
+                                                <h2>{movie.title}</h2>
+                                            </a>
 
-                <td>
-                    <p>{movie.runtime}</p> 
-                    
-                </td>
+                                            <p>
+                                                {movie.description}
+                                            </p>
+                                        </td>
 
-                <td>
-                    <p>{movie.votes}</p> 
-                    
-                </td>
+                                        <td>
+                                            <p>{movie.year}</p>
 
-                <td>
-                    <p>{movie.genres}</p> 
-                    
-                </td>
+                                        </td>
 
-            </tr>
-            ))
-        } 
-        </table>
+                                        <td>
+                                            <p>{movie.rating}</p>
+
+                                        </td>
+
+                                        <td>
+                                            <p>{movie.runtimeMins}</p>
+
+                                        </td>
+
+                                        <td>
+                                            <p>{movie.votes}</p>
+
+                                        </td>
+
+                                        <td>
+                                            <p>{movie.genres}</p>
+
+                                        </td>
+
+                                    </tr>
+                                ))
+                    }
+                </tbody>
+            </table>
         </>
     )
 }
